e1d1e00bcd094400512b04979ae06d5c6bafcfc0
Juliano Boese <juliano.boese@gmail.com>
2021-11-24 16:33:11 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+const colors = document.getElementsByClassName('ball');
+const guessAnswer = document.getElementById('answer');
+const resetButton = document.getElementById('reset-game');
+
 function generateColors() {
-  const colors = document.getElementsByClassName('ball');
   for (let i = 0; i < colors.length; i += 1) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     colors[i].style.backgroundColor = `#${randomColor}`;
   }
+
+  const rgbIndex = Math.floor(Math.random() * (colors.length - 1));
+  document.getElementById('rgb-color').innerHTML = colors[rgbIndex].style.backgroundColor.slice(3);
 }
 
 window.onload = generateColors;
+
+function checkAnswer() {
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].addEventListener('click', (event) => {
+      const colorToGuess = `rgb${document.getElementById('rgb-color').innerHTML}`;
+      if (event.target.style.backgroundColor === colorToGuess) {
+        guessAnswer.innerHTML = 'Acertou!';
+      } else {
+        guessAnswer.innerHTML = 'Errou! Tente novamente!';
+      }
+    });
+  }
+}
+
+checkAnswer();
+
+function resetColors() {
+  resetButton.addEventListener('click', () => {
+    generateColors();
+    guessAnswer.innerHTML = 'Escolha uma cor';
+  });
+}
+
+resetColors();
